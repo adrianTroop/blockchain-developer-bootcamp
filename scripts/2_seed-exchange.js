c6a035ef786acf810d6ab4178b0b5940ba41b538
Adrian <adrian@adriantroop.com>
2023-04-04 17:05:16 +0200

    seeding 1

---
+++ scripts/2_seed-exchange.js
@@ -0,0 +1,20 @@
+const { ethers } = require("hardhat");
+
+async function main() {
+    //Distribute Tokens
+
+    //Deposit tokens to exchange
+
+    //Make orders
+
+    //Cancel orders
+
+    //Fill orders
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
