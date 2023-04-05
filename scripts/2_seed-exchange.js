36596f6a586a5884ff0c0735879f4cc5b55ae953
Adrian <adrian@adriantroop.com>
2023-04-05 15:01:10 +0200

    react explain

--- scripts/2_seed-exchange.js
+++ scripts/2_seed-exchange.js
@@ -1,4 +1,5 @@
 const { ethers } = require("hardhat");
+const config = require('../src/config.json')
 
 const tokens = (n) => {
     return ethers.utils.parseUnits(n.toString(), "ether")
@@ -9,21 +10,28 @@ const wait = (seconds) => {
     return new Promise(resolve => setTimeout(resolve, milliseconds))
 }
 
+//Being specific with your chainID
+
+
 async function main() {
+
+    const {chainId} = await ethers.provider.getNetwork()
+    console.log("Using chainid", chainId)
+
     //get accounts
     const  accounts = await ethers.getSigners()
     //Fetched tokens
-    const Dapp = await ethers.getContractAt('Token', '0x5FbDB2315678afecb367f032d93F642f64180aa3')
+    const Dapp = await ethers.getContractAt('Token', config[chainId].Dapp.address)
     console.log(`Dapp token fetched: ${Dapp.address}\n`)
 
-    const mETH = await ethers.getContractAt('Token', '0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512')
-    console.log(`Dapp token fetched: ${mETH.address}\n`)
+    const mETH = await ethers.getContractAt('Token', config[chainId].mETH.address)
+    console.log(`mETH token fetched: ${mETH.address}\n`)
 
-    const mDAI = await ethers.getContractAt('Token', '0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0')
-    console.log(`Dapp token fetched: ${mDAI.address}\n`)
+    const mDAI = await ethers.getContractAt('Token', config[chainId].mDAI.address)
+    console.log(`mDAI token fetched: ${mDAI.address}\n`)
 
-    const exchange = await ethers.getContractAt('Exchange', '0xCf7Ed3AccA5a467e9e704C703E8D87F634fB0Fc9')
-    console.log(`Dapp token fetched: ${exchange.address}\n`)
+    const exchange = await ethers.getContractAt('Exchange', config[chainId].exchange.address)
+    console.log(`Exchange fetched: ${exchange.address}\n`)
     
     //Implement sender and receiver
     const sender = accounts[0]
@@ -47,14 +55,14 @@ async function main() {
     result = await transaction.wait()
     transaction = await exchange.connect(user1).depositToken(Dapp.address, amount)
     result = await transaction.wait()
-    console.log(`${user1.address} depostied ${amount} into ${exchange.address}\n`)
+    console.log(`${user1.address} deposited ${amount} into ${exchange.address}\n`)
 
     //user 2 deposit 1000 mETH into the exchange
     transaction = await mETH.connect(user2).approve(exchange.address, amount)
     result = await transaction.wait()
     transaction = await exchange.connect(user2).depositToken(mETH.address, amount)
     result = await transaction.wait()
-    console.log(`${user2.address} depostied ${amount} into ${exchange.address}\n`)
+    console.log(`${user2.address} deposited ${amount} into ${exchange.address}\n`)
 
     //////////////////////////////////////////////////
     //Make and cancel orders
